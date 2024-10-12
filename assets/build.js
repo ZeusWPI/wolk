c683147481ef231e7d12c6165a75e8d755c3468c
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-10-12 23:40:55 +0200

    chore: use latest esbuild

--- assets/build.js
+++ assets/build.js
@@ -4,10 +4,10 @@ const importGlobPlugin = require("esbuild-plugin-import-glob").default
 const sveltePreprocess = require("svelte-preprocess")
 
 const args = process.argv.slice(2)
-const watch = args.includes("--watch")
 const deploy = args.includes("--deploy")
+const watch = process.argv.includes("--watch")
 
-let optsClient = {
+let context = {
   entryPoints: ["js/app.js"],
   bundle: true,
   minify: deploy,
@@ -16,49 +16,69 @@ let optsClient = {
   outdir: "../priv/static/assets",
   logLevel: "info",
   sourcemap: watch ? "inline" : false,
-  watch,
   tsconfig: "./tsconfig.json",
   plugins: [
     importGlobPlugin(),
     sveltePlugin({
-      preprocess: sveltePreprocess(),
+      preprocess: sveltePreprocess({ typescript: true }),
       compilerOptions: { dev: !deploy, hydratable: true, css: "injected" },
     }),
   ],
 }
 
-let optsServer = {
+let ssrContext = {
   entryPoints: ["js/server.js"],
   platform: "node",
   bundle: true,
   minify: false,
-  target: "node22.8.0",
+  target: "node19.6.1",
   conditions: ["svelte"],
   outdir: "../priv/svelte",
   logLevel: "info",
   sourcemap: watch ? "inline" : false,
-  watch,
   tsconfig: "./tsconfig.json",
   plugins: [
     importGlobPlugin(),
     sveltePlugin({
-      preprocess: sveltePreprocess(),
+      preprocess: sveltePreprocess({ typescript: true }),
       compilerOptions: { dev: !deploy, hydratable: true, generate: "ssr" },
     }),
   ],
 }
 
-const client = esbuild.build(optsClient)
-const server = esbuild.build(optsServer)
+async function watchBuild() {
+  let ctx = await esbuild.context(context);
+  await ctx.watch();
+  let ssrCtx = await esbuild.context(ssrContext);
+  await ssrCtx.watch();
+  console.log("ESBuild is watching for changes...");
+}
 
 if (watch) {
-  client.then(_result => {
-    process.stdin.on("close", () => process.exit(0))
-    process.stdin.resume()
-  })
+  watchBuild();
+} else {
+  esbuild
+    .build({
+      minify: true,
+      treeShaking: true,
+      ...context,
+    })
+    .catch(() => process.exit(1));
 
-  server.then(_result => {
-    process.stdin.on("close", () => process.exit(0))
-    process.stdin.resume()
-  })
+  esbuild.build(ssrContext).catch(() => process.exit(1));
 }
+
+// const client = esbuild.build(optsClient)
+// const server = esbuild.build(optsServer)
+//
+// if (watch) {
+//   client.then(_result => {
+//     process.stdin.on("close", () => process.exit(0))
+//     process.stdin.resume()
+//   })
+//
+//   server.then(_result => {
+//     process.stdin.on("close", () => process.exit(0))
+//     process.stdin.resume()
+//   })
+// }
